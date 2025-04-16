b0c12690b4008ee042258c2e71a267c6d6f2d783
Jazmin Torrez <jazz.tb.99@gmail.com>
2025-04-16 00:08:58 -0400

    sidebar mejorado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { UsuarioComponent } from './pages/usuario/usuario.component';
+//import { ProductoComponent } from './pages/productos/producto.component';
+//import { VentasComponent } from './pages/ventas/ventas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'usuario', component: UsuarioComponent },
+ // { path: 'productos', component: ProductoComponent },
+ // { path: 'ventas', component: VentasComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
