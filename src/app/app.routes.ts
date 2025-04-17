0007cc7139e2126431555114a022097c4c8c2c23
Diana-GR <guzman.dgr00@gmail.com>
2025-04-17 00:33:08 -0400

    camdios en el sidebar y routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
+import { SubcategoriasComponent } from './pages/subcategorias/subcategorias.component';
+import CategoriasComponent from './pages/categorias/categorias.component';
+
 //import { ProductoComponent } from './pages/productos/producto.component';
 //import { VentasComponent } from './pages/ventas/ventas.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'usuario', component: UsuarioComponent },
- // { path: 'productos', component: ProductoComponent },
- // { path: 'ventas', component: VentasComponent },
+  // { path: 'productos', component: ProductoComponent },
+  // { path: 'ventas', component: VentasComponent },
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'subcategorias', component: SubcategoriasComponent },
+  // { path: 'materias_primas', component: MateriasPrimasComponent },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
