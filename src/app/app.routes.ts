7a5d729d143f46598d04c6375afbd3241a65b90e
Diana-GR <guzman.dgr00@gmail.com>
2025-04-27 01:46:23 -0400

    add: landing-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,18 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { UsuarioComponent } from './pages/usuario/usuario.component';
 import { RolComponent } from './pages/rol/rol.component';
-//import { ProductoComponent } from './pages/productos/producto.component';
-//import { VentasComponent } from './pages/ventas/ventas.component';
+import CategoriasComponent from './pages/categorias/categorias.component';
+import { SubcategoriasComponent } from './pages/subcategorias/subcategorias.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'usuario', component: UsuarioComponent },
   { path: 'roles', component: RolComponent },
- // { path: 'productos', component: ProductoComponent },
- // { path: 'ventas', component: VentasComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: 'categorias', component: CategoriasComponent },
+  { path: 'subcategorias', component: SubcategoriasComponent },
+  { path: 'mrp', component: LandingPageComponent },
+  // { path: '', redirectTo: 'login', pathMatch: 'full' },  //anterior pagina de inicio por defecto
+  { path: '', redirectTo: 'mrp', pathMatch: 'full' },
+  { path: '**', redirectTo: '/mrp' }
 ];
--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://143.198.143.11:8080/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
-    // apiUrl: 'http://localhost:8080/mrp/'
+    // apiUrl: 'http://143.198.143.11:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
+    apiUrl: 'http://localhost:8081/mrp/'
 };
