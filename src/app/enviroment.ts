9319d5a87bd2ed9698709dcd2909e9f23573fd35
Shirley <gutierrez.shirley@ficct.uagrm.edu.bo>
2025-05-28 22:27:24 -0400

    add export reportes CSV

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://143.110.145.162:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
-    //apiUrl: 'http://localhost:8081/mrp/'
+    //apiUrl: 'http://143.110.145.162:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
+    apiUrl: 'http://localhost:8081/mrp/'
     
 };
