bfc8d841fd4849a21216ba0191ab7eb21cb52ad0
Job Natanael Contreras Garzon <143237173+Job-Natanael-Contreras-Garzon@users.noreply.github.com>
2025-06-27 20:07:47 -0400

    update url

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://198.199.83.4:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
-    apiUrl: 'http://localhost:8081/mrp/',
+    apiUrl: 'https://concerned-annnora-davv-632fa209.koyeb.app/',
       // Configuración de Stripe
     stripePublicKey: 'pk_test_51RbW0M2ekOb04oTVyGnNI0OoBkIzl66nkwgKoFpLe2LpB40O0kRyDxQSlUHajTs7D6NBSRpNHOIHQUpKNwTCubYR00UjRpGiLB' // Reemplaza con tu clave pública real de Stripe
 };
