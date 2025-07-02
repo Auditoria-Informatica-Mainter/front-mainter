f91c75e8f3eef0489d12c92f1aad8b234abb26a5
Alan <alan.melgar.f@gmail.com>
2025-07-02 00:35:46 -0400

    enviroment

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     // apiUrl: 'http://198.199.83.4:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
-    apiUrl: 'https://concerned-annnora-davv-632fa209.koyeb.app/mrp/',
+    // apiUrl: 'https://concerned-annnora-davv-632fa209.koyeb.app/mrp/',
+    apiUrl: 'https://back-mainter.onrender.com/mrp/',
       // Configuración de Stripe
     stripePublicKey: 'pk_test_51RbW0M2ekOb04oTVyGnNI0OoBkIzl66nkwgKoFpLe2LpB40O0kRyDxQSlUHajTs7D6NBSRpNHOIHQUpKNwTCubYR00UjRpGiLB' // Reemplaza con tu clave pública real de Stripe
 };
