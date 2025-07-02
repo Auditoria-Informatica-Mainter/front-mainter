36e0c04cb83ad646dcb4426e2ff9fc67d675d5ef
Alan <alan.melgar.f@gmail.com>
2025-07-02 19:39:28 -0400

    back

--- src/app/enviroment.ts
+++ src/app/enviroment.ts
@@ -2,7 +2,8 @@ export const environment = {
     production: true,
     // apiUrl: 'http://198.199.83.4:8081/mrp/' //MODIFICAR EL PUERTO DE SER NECESARIO
     // apiUrl: 'https://concerned-annnora-davv-632fa209.koyeb.app/mrp/',
-    apiUrl: 'https://back-mainter.onrender.com/mrp/',
+    // apiUrl: 'https://back-mainter.onrender.com/mrp/',
+    apiUrl: 'https://calm-denice-alan-alvis-f-fd856a4b.koyeb.app/mrp/',
       // Configuración de Stripe
     stripePublicKey: 'pk_test_51RbW0M2ekOb04oTVyGnNI0OoBkIzl66nkwgKoFpLe2LpB40O0kRyDxQSlUHajTs7D6NBSRpNHOIHQUpKNwTCubYR00UjRpGiLB' // Reemplaza con tu clave pública real de Stripe
 };
